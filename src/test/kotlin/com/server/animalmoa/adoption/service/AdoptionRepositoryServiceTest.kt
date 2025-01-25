78ab67eb80710b5cae2b72f8ed5bd07556e512a8
starwook <star.wookk@gmail.com>
2025-01-26 01:20:11 +0900

    test: 분양글 생성, 검색, 조회 테스트 구현

---
+++ src/test/kotlin/com/server/animalmoa/adoption/service/AdoptionRepositoryServiceTest.kt
@@ -0,0 +1,35 @@
+package com.server.animalmoa.adoption.service
+
+import com.server.animalmoa.adoption.data.MakeAdoptionDto
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+@DisplayName("분양글 Repository")
+class AdoptionRepositoryServiceTest {
+    @Autowired
+    lateinit var adoptionRepositoryService: AdoptionRepositoryService
+
+    @Test
+    @DisplayName("분양글 생성, 검색, 삭제")
+    fun saveAndDeleteAdoption() {
+        // given
+        val adoptionDto = MakeAdoptionDto.forTest()
+        // when
+        val adoption = adoptionRepositoryService.save(adoptionDto)
+        println(adoption)
+        val adoptionSaved = adoptionRepositoryService.findById(adoption.id)
+        println(adoptionSaved)
+        // then
+        Assertions.assertThat(adoptionRepositoryService.findById(adoption.id)).isNotNull
+
+        //  when
+        adoptionRepositoryService.delete(adoption)
+        val adoptionDeleted = adoptionRepositoryService.findById(adoption.id)
+        // then
+        Assertions.assertThat(adoptionDeleted).isNull()
+    }
+}
