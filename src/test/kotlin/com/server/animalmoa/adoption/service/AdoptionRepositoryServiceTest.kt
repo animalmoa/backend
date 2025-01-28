65037dfa33d9a951dc99ad518d81ac76635010fc
starwook <star.wookk@gmail.com>
2025-01-28 20:47:28 +0900

    test: AdoptionRepositoryServiceTest 수정

--- src/test/kotlin/com/server/animalmoa/adoption/service/AdoptionRepositoryServiceTest.kt
+++ src/test/kotlin/com/server/animalmoa/adoption/service/AdoptionRepositoryServiceTest.kt
@@ -1,7 +1,7 @@
 package com.server.animalmoa.adoption.service
 
 import com.server.animalmoa.adoption.data.MakeAdoptionDto
-import com.server.animalmoa.adoption.repository.AdoptionRepository
+import com.server.animalmoa.adoption.domain.Adoption
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -12,29 +12,26 @@ import org.springframework.boot.test.context.SpringBootTest
 @DisplayName("분양글 Repository")
 class AdoptionRepositoryServiceTest {
     @Autowired
-    private lateinit var adoptionRepository: AdoptionRepository
-
-    @Autowired
     lateinit var adoptionRepositoryService: AdoptionRepositoryService
 
     @Test
     @DisplayName("분양글 생성, 검색, 삭제")
     fun saveAndDeleteAdoption() {
         // given
-        val adoptionDto = MakeAdoptionDto.forTest()
+        val adoption = Adoption.from(MakeAdoptionDto.forTest())
         // when
-        val adoption = adoptionRepositoryService.save(adoptionDto)
-        println(adoption)
-        val adoptionSaved = adoptionRepositoryService.findById(adoption?.id)
+        val adoptionFromDb = adoptionRepositoryService.save(adoption)
+        println(adoptionFromDb)
+        val adoptionSaved = adoptionRepositoryService.findById(adoptionFromDb?.id)
         println(adoptionSaved)
         // then
-        Assertions.assertThat(adoptionRepositoryService.findById(adoption?.id)).isNotNull
+        Assertions.assertThat(adoptionRepositoryService.findById(adoptionFromDb?.id)).isNotNull
 
         //  when
-        adoption?.let {
+        adoptionFromDb?.let {
             adoptionRepositoryService.delete(it)
         }
-        val adoptionDeleted = adoptionRepositoryService.findById(adoption?.id)
+        val adoptionDeleted = adoptionRepositoryService.findById(adoptionFromDb?.id)
         // then
         Assertions.assertThat(adoptionDeleted).isNull()
     }
