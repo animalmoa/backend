3a4148f7dcada2ebba62643916be70b07647efb9
starwook <star.wookk@gmail.com>
2025-02-25 22:32:00 +0900

    refactor: 각 게시글의 에러 핸들링을 crawlerErrorService에서 수행하도록 구현

--- src/main/kotlin/com/server/animalmoa/crawler/source/animalgo/AnimalGoCrawler.kt
+++ src/main/kotlin/com/server/animalmoa/crawler/source/animalgo/AnimalGoCrawler.kt
@@ -5,7 +5,7 @@ import com.server.animalmoa.adoption.domain.AdoptionStatus
 import com.server.animalmoa.adoption.domain.Source
 import com.server.animalmoa.common.PostType
 import com.server.animalmoa.crawler.service.AdoptionCrawler
-import com.server.animalmoa.exception.DataParseException
+import com.server.animalmoa.crawler.service.CrawlerErrorService
 import com.server.animalmoa.webdriver.WebDriverCommandService
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
@@ -19,6 +19,7 @@ TODO 실종동물 페이지
 class AnimalGoCrawler(
     private val webDriverCommandService: WebDriverCommandService,
     private val animalGoDataManageService: AnimalGoDataManageService,
+    private val crawlerErrorService: CrawlerErrorService,
 ) : AdoptionCrawler {
     private val logger = KotlinLogging.logger {}
 
@@ -40,38 +41,37 @@ class AnimalGoCrawler(
     private fun searchEachPage(freeAdoptionPath: AnimalGoData) {
         var animals = webDriverCommandService.findElementsWithWaitingAlwaysAsList(freeAdoptionPath.animalsXpath)
         for (index in animals.indices) {
-            try {
-                // 매번 창은 초기화 되기 때문에 새로 검색해 주어야함
-                animals = webDriverCommandService.findElementsWithWaitingAlwaysAsList(freeAdoptionPath.animalsXpath)
-                if (index >= animals.size) break
-                webDriverCommandService.clickElementWithAction(animals[index])
-                animalGoDataManageService.parseDataAndSave(
-                    MakeAdoptionDto(
-                        species = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.essential.speciesXpath)?.text,
-                        breed = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.essential.breedXpath)?.text,
-                        region = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.essential.regionXpath)?.text,
-                        gender = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.essential.genderXpath)?.text,
-                        title = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.essential.titleXpath)?.text,
-                        content = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.essential.contentXpath)?.text,
-                        age = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.essential.ageXpath)?.text,
-                        createdAt = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.createdAtXpath)?.text,
-                        thumbnailUrl =
-                            webDriverCommandService
-                                .findElementWithWaiting(freeAdoptionPath.essential.thumbnailXpath)
-                                ?.getAttribute("src"),
-                        postType = PostType.FREE_ADOPTION.name,
-                        adoptionStatus = AdoptionStatus.ING.name,
-                        originalUrl = webDriverCommandService.getWebDriver().currentUrl,
-                        source = Source.ANIMAL_GO,
-                        identifier = webDriverCommandService.getWebDriver().currentUrl,
-                    ),
-                )
-                webDriverCommandService.goBack()
-            } catch (e: DataParseException) {
-                logger.error { e.printStackTrace() }
-            } catch (e: Exception) {
-                logger.error { e.printStackTrace() }
-            }
+            // 매번 창은 초기화 되기 때문에 새로 검색해 주어야함
+            animals = webDriverCommandService.findElementsWithWaitingAlwaysAsList(freeAdoptionPath.animalsXpath)
+            if (index >= animals.size) break
+            crawlerErrorService.catchCrawlError(
+                {
+                    webDriverCommandService.clickElementWithAction(animals[index])
+                    animalGoDataManageService.parseDataAndSave(
+                        MakeAdoptionDto(
+                            species = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.essential.speciesXpath)?.text,
+                            breed = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.essential.breedXpath)?.text,
+                            region = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.essential.regionXpath)?.text,
+                            gender = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.essential.genderXpath)?.text,
+                            title = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.essential.titleXpath)?.text,
+                            content = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.essential.contentXpath)?.text,
+                            age = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.essential.ageXpath)?.text,
+                            createdAt = webDriverCommandService.findElementWithWaiting(freeAdoptionPath.createdAtXpath)?.text,
+                            thumbnailUrl =
+                                webDriverCommandService
+                                    .findElementWithWaiting(freeAdoptionPath.essential.thumbnailXpath)
+                                    ?.getAttribute("src"),
+                            postType = PostType.FREE_ADOPTION.name,
+                            adoptionStatus = AdoptionStatus.ING.name,
+                            originalUrl = webDriverCommandService.getWebDriver().currentUrl,
+                            source = Source.ANIMAL_GO,
+                            identifier = webDriverCommandService.getWebDriver().currentUrl,
+                        ),
+                    )
+                    webDriverCommandService.goBack()
+                },
+                logger,
+            )
         }
     }
 }
