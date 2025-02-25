e4718488fa9e96eb894aba1be8f022b9130d1f35
starwook <star.wookk@gmail.com>
2025-02-25 23:41:42 +0900

    refactor:

--- src/main/kotlin/com/server/animalmoa/webdriver/ScreenShotCaptureService.kt
+++ src/main/kotlin/com/server/animalmoa/webdriver/ScreenShotCaptureService.kt
@@ -1,20 +1,25 @@
 package com.server.animalmoa.webdriver
 
 import com.server.animalmoa.oracle.OciObjectStorageService
+import org.openqa.selenium.JavascriptExecutor
 import org.openqa.selenium.OutputType
 import org.openqa.selenium.WebElement
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class ScreenShotCaptureService(
     private val ociObjectStorageService: OciObjectStorageService,
+    private val webDriverManager: WebDriverManager,
 ) {
     fun getScreenShot(screenShotElement: WebElement?): String? {
         if (screenShotElement == null) return null
-        // 네이버 카페의 알림창 들을 사라지게 하기 위한 최소한의 시간
-        Thread.sleep(2000)
+        // 요소를 화면 상단에 위치시키도록 스크롤
+        (webDriverManager.getWebDriver() as JavascriptExecutor).executeScript("arguments[0].scrollIntoView(true);", screenShotElement)
+        // 스크롤 완료 후 안정화를 위해 잠시 대기
+        Thread.sleep(500)
         return screenShotElement.getScreenshotAs(OutputType.BYTES).let { bytes ->
-            val fileName = "screenshot-${System.currentTimeMillis()}.png"
+            val fileName = "screenshot-${LocalDateTime.now()}.png"
             val ociUrl = ociObjectStorageService.uploadImageAsByteArray(fileName, bytes)
             ociUrl
         }
