c0e99b316d901bd3d1e4557ab83e7cc7d6c12789
starwook <star.wookk@gmail.com>
2025-03-11 22:55:08 +0900

    feat: 모듈에 따른 gradle 설정 분리

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,60 +1,65 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
-    kotlin("plugin.jpa") version "1.8.0"
-    kotlin("plugin.spring") version "1.9.25"
+    id("org.jetbrains.kotlin.jvm") version "1.8.0"
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.8.0"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "2.7.10"
     id("io.spring.dependency-management") version "1.1.6"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
+allprojects {
+    group = "com.server.animalmoa"
+    version = "0.0.1-SNAPSHOT"
 
-group = "com.server.animalmoa"
-version = "0.0.1-SNAPSHOT"
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+    // Java toolchain 설정 (각 서브모듈에 적용)
+    repositories {
+        mavenCentral()
     }
 }
 
-repositories {
-    mavenCentral()
-}
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // 공통 의존성 설정
+    dependencies {
+        implementation("io.github.microutils:kotlin-logging:3.0.5")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-    implementation("org.seleniumhq.selenium:selenium-java:4.28.1")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("io.github.microutils:kotlin-logging:3.0.5")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        runtimeOnly("com.mysql:mysql-connector-j")
+        runtimeOnly("com.h2database:h2")
 
-    /*
-    Oracle Cloud
-     */
-    implementation("com.oracle.oci.sdk:oci-java-sdk:3.38.0")
-    implementation("com.oracle.oci.sdk:oci-java-sdk-common:3.38.0")
-    implementation("com.oracle.oci.sdk:oci-java-sdk-objectstorage:3.38.0")
-    implementation("com.oracle.oci.sdk:oci-java-sdk-common-httpclient:3.38.0")
-    implementation("com.oracle.oci.sdk:oci-java-sdk-common-httpclient-choices:3.38.0")
-    implementation("com.oracle.oci.sdk:oci-java-sdk-common-httpclient-jersey:3.38.0")
-    implementation("com.oracle.oci.sdk:oci-java-sdk-shaded-full:3.38.0")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("com.mysql:mysql-connector-j")
-}
-
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
-}
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+    // Kotlin 컴파일러 옵션 설정
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+        }
+    }
+    tasks.register("prepareKotlinBuildScriptModel") {}
+    // 테스트 설정
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(17)
+        }
+    }
+
+    kotlin {
+        jvmToolchain {
+            languageVersion = JavaLanguageVersion.of(17)
+        }
+    }
 }
