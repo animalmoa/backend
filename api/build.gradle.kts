593859210a45fc153c03e66fdc1064ad3745d17b
starwook <star.wookk@gmail.com>
2025-04-22 21:28:14 +0900

    build: api 모듈에서 common모듈의 설정을 수입하도록 수현

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,3 +3,15 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 }
+
+// common 모듈의 리소스를 api의 bootJar에 포함시키기
+tasks.named<Jar>("bootJar") {
+    doFirst {
+        val deployDbFile = file("../common/src/main/resources/application-deploy-db.yml")
+        if (!deployDbFile.exists()) {
+            throw GradleException("❌ application-deploy-db.yml 파일이 존재하지 않습니다!")
+        } else {
+            println("✅ application-deploy-db.yml 파일 확인 완료")
+        }
+    }
+}
