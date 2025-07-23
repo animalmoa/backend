63b08dda6e3a732fa6a913057c1fb5ebf873c941
유성욱 <88507708+starwook@users.noreply.github.com>
2025-07-23 23:49:10 +0900

    스크래핑시의 공통 비즈니스 로직을 분리, 공통 에러 처리 분리 (#24)

--- crawler/src/main/kotlin/com/server/animalmoa/crawler/scraper/source/animalgo/AnimalGoScraper.kt
+++ crawler/src/main/kotlin/com/server/animalmoa/crawler/scraper/source/animalgo/AnimalGoScraper.kt
@@ -1,12 +1,12 @@
 package com.server.animalmoa.crawler.scraper.source.animalgo
 
 import com.server.animalmoa.common.adoption.enum.Source
-import com.server.animalmoa.crawler.exception.AlreadySavedPostException
 import com.server.animalmoa.crawler.scraper.service.AdoptionScraper
+import com.server.animalmoa.crawler.scraper.service.ScraperErrorService
 import com.server.animalmoa.crawler.scraper.threadmanager.AdoptionSaveManager
-import com.server.animalmoa.crawler.scraper.threadmanager.AdoptionToSave
 import com.server.animalmoa.crawler.webdriver.WebDriverCommandService
 import mu.KotlinLogging
+import org.openqa.selenium.WebElement
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
@@ -16,10 +16,12 @@ import org.springframework.stereotype.Service
   */
 @Service
 class AnimalGoScraper(
-    private val webDriverCommandService: WebDriverCommandService,
-    private val adoptionSaveManager: AdoptionSaveManager,
-) : AdoptionScraper {
-    private val logger = KotlinLogging.logger {}
+    webDriverCommandService: WebDriverCommandService,
+    adoptionSaveManager: AdoptionSaveManager,
+    scraperErrorService: ScraperErrorService,
+) : AdoptionScraper(webDriverCommandService, adoptionSaveManager, scraperErrorService) {
+    override val source = Source.ANIMAL_GO
+    override val logger = KotlinLogging.logger { source }
 
     @Value("\${scrap-until.page}")
     private val maxPage: Int = 10
@@ -27,39 +29,38 @@ class AnimalGoScraper(
     override fun scrapAdoptionPost() {
         val htmlParser = AnimalGoAdoptionHtmlParser.adoption()
         for (page in 1..maxPage) {
-            val freeAdoptionUrl =
+            val postUrl =
                 "https://www.animal.go.kr/front/awtis/protection/protectionList.do?" +
                     "menuNo=${htmlParser.menuNoParam}" +
                     "&page=$page"
-            webDriverCommandService.navigateTo(freeAdoptionUrl)
-            searchEachPage(htmlParser)
+
+            scraperErrorService.catchScrawlPostListError(
+                logger,
+            ) {
+                webDriverCommandService.navigateTo(postUrl)
+                val postElements = webDriverCommandService.findElementsWithWaitingAlwaysAsList(htmlParser.postXpathes)
+
+                postElements.forEach { element ->
+                    scrapEachPost(htmlParser, element)
+                }
+            }
         }
     }
 
-    private fun searchEachPage(animalGoAdoptionHtmlParser: AnimalGoAdoptionHtmlParser) {
-        val eachPostElements = webDriverCommandService.findElementsWithWaitingAlwaysAsList(animalGoAdoptionHtmlParser.postXpathes)
-        eachPostElements.forEach { element ->
-            val eachPostIdentifier = animalGoAdoptionHtmlParser.postIdentifier(element.getAttribute("onclick"))
-            if (eachPostIdentifier == null) {
-                logger.error { "extracting identifier fail" }
-            } else {
-                val eachPostUrl = animalGoAdoptionHtmlParser.postUrl(eachPostIdentifier)
-                if (adoptionSaveManager.isNewPost(Source.ANIMAL_GO, eachPostIdentifier)) {
-                    adoptionSaveManager.addAdoptionToQueue(
-                        AdoptionToSave(
-                            eachPostUrl,
-                            {
-                                animalGoAdoptionHtmlParser.getMakeAdoptionDto(
-                                    webDriverCommandService.getHtml(eachPostUrl),
-                                    eachPostIdentifier,
-                                )
-                            },
-                            AdoptionToSave.NEW_POST_PRIORITY,
-                        ),
-                    )
-                } else {
-                    throw AlreadySavedPostException(eachPostUrl)
-                }
+    private fun scrapEachPost(
+        htmlParser: AnimalGoAdoptionHtmlParser,
+        element: WebElement,
+    ) {
+        scraperErrorService.catchScrawlPostError(
+            logger,
+        ) {
+            val identifier = htmlParser.postIdentifier(element.getAttribute("onclick"))
+            val postUrl = identifier?.let { htmlParser.postUrl(it) }
+            scrapNewPost(identifier, postUrl) {
+                htmlParser.getMakeAdoptionDto(
+                    webDriverCommandService.getHtml(postUrl!!),
+                    identifier,
+                )
             }
         }
     }
--- crawler/src/main/kotlin/com/server/animalmoa/crawler/scraper/source/juseyo/JuseyoScraper.kt
+++ crawler/src/main/kotlin/com/server/animalmoa/crawler/scraper/source/juseyo/JuseyoScraper.kt
@@ -1,82 +1,76 @@
 package com.server.animalmoa.crawler.scraper.source.juseyo
 
 import com.server.animalmoa.common.adoption.enum.Source
-import com.server.animalmoa.crawler.exception.AlreadySavedPostException
 import com.server.animalmoa.crawler.scraper.service.AdoptionScraper
+import com.server.animalmoa.crawler.scraper.service.ScraperErrorService
 import com.server.animalmoa.crawler.scraper.threadmanager.AdoptionSaveManager
-import com.server.animalmoa.crawler.scraper.threadmanager.AdoptionToSave
 import com.server.animalmoa.crawler.webdriver.WebDriverCommandService
 import mu.KotlinLogging
+import org.openqa.selenium.WebElement
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Suppress("ktlint:standard:no-consecutive-comments")
 @Service
 class JuseyoScraper(
-    private val webDriverCommandService: WebDriverCommandService,
-    private val adoptionSaveManager: AdoptionSaveManager,
-) : AdoptionScraper {
-    val logger = KotlinLogging.logger {}
+    webDriverCommandService: WebDriverCommandService,
+    adoptionSaveManager: AdoptionSaveManager,
+    scraperErrorService: ScraperErrorService,
+) : AdoptionScraper(webDriverCommandService, adoptionSaveManager, scraperErrorService) {
+    override val source: Source = Source.JUSEYO
+    override val logger = KotlinLogging.logger { source }
 
     @Value("\${scrap-until.page}")
     private val maxPage: Int = 10
 
     override fun scrapAdoptionPost() {
-        val animalCategories =
+        val htmlParserByCategory =
             listOf(
                 JuseyoAdoptionHtmlParser.cat(),
                 JuseyoAdoptionHtmlParser.dog(),
             )
 
-        animalCategories.forEach category@{ animalCategory ->
+        htmlParserByCategory.forEach category@{ htmlParser ->
             (1..maxPage).forEach page@{ page ->
                 val eachPageOfCategory =
                     "https://www.zooseyo.com/sale/sale_list.php" +
-                        "?animal=${animalCategory.animalParam}&page=$page" +
-                        "&category=${animalCategory.categoryParam}&kind=&area=&categoryetc="
-                try {
-                    webDriverCommandService.navigateTo(eachPageOfCategory)
-                    try {
-                        searchEachCategory(animalCategory)
-                    } catch (e: AlreadySavedPostException) {
-                        logger.error { "stop scraping ${animalCategory.species} because ${e.message}" }
-                        // 이미 있는 글이란 에러를 받았을 경우 다음 카테고리로 넘어간다.
-                        return@category
+                        "?animal=${htmlParser.animalParam}&page=$page" +
+                        "&category=${htmlParser.categoryParam}&kind=&area=&categoryetc="
+
+                runCatching {
+                    scraperErrorService.catchScrawlPostListError(
+                        logger,
+                    ) {
+                        webDriverCommandService.navigateTo(eachPageOfCategory)
+                        val postElements = webDriverCommandService.findElementsWithWaitingAlwaysAsList(htmlParser.postXpathes)
+
+                        postElements.forEach { element ->
+                            scrapEachPost(htmlParser, element)
+                        }
                     }
-                } catch (e: Exception) {
-                    // 카테고리 각 페이지 글에 진입 못 했을 경우
-                    // 다음 페이지 진입 가능성도 낮기에, 카테고리 변경
-                    return@category
                 }
+                    // 게시글 리스트 페이지 실패시 다음 카테고리 시도.
+                    .onFailure { return@category }
             }
         }
     }
 
-    private fun searchEachCategory(juseyoAdoptionHtmlParser: JuseyoAdoptionHtmlParser) {
-        val postElements = webDriverCommandService.findElementsWithWaitingAlwaysAsList(juseyoAdoptionHtmlParser.postXpathes)
+    private fun scrapEachPost(
+        htmlParser: JuseyoAdoptionHtmlParser,
+        element: WebElement,
+    ) {
+        scraperErrorService.catchScrawlPostError(
+            logger,
+        ) {
+            val postUri = htmlParser.postUrl(element.getAttribute("onclick"))
+            val postUrl = postUri?.let { Source.JUSEYO.url + it }
+            val identifier = postUrl?.let { JuseyoAdoptionHtmlParser.getIdentifier(it) }
 
-        postElements.forEach { element ->
-            val eachPostUri = juseyoAdoptionHtmlParser.postUrl(element.getAttribute("onclick"))
-            if (eachPostUri == null) {
-                logger.error { "extracting post url fail " }
-            } else {
-                val eachPostUrl = Source.JUSEYO.url + eachPostUri
-                if (adoptionSaveManager.isNewPost(Source.JUSEYO, JuseyoAdoptionHtmlParser.getIdentifier(eachPostUrl))) {
-                    adoptionSaveManager.addAdoptionToQueue(
-                        AdoptionToSave(
-                            eachPostUrl,
-                            {
-                                juseyoAdoptionHtmlParser.getMakeAdoptionDto(
-                                    webDriverCommandService.getHtml(eachPostUrl),
-                                    eachPostUrl,
-                                )
-                            },
-                            AdoptionToSave.NEW_POST_PRIORITY,
-                        ),
-                    )
-                } else {
-                    throw AlreadySavedPostException(eachPostUrl)
-                }
+            scrapNewPost(identifier, postUrl) {
+                htmlParser.getMakeAdoptionDto(
+                    webDriverCommandService.getHtml(postUrl!!),
+                    postUrl,
+                )
             }
         }
     }
