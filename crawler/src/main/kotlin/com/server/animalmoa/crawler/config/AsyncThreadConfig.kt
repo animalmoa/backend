74db05fe34dc839e049f69a8e0af869f254e55e3
starwook <star.wookk@gmail.com>
2025-07-23 20:58:16 +0900

    feat: 에러 추적 강화

--- crawler/src/main/kotlin/com/server/animalmoa/crawler/config/AsyncThreadConfig.kt
+++ crawler/src/main/kotlin/com/server/animalmoa/crawler/config/AsyncThreadConfig.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.ThreadPoolExecutor
 
 @Configuration
 @EnableAsync
-class ScrapAsyncThreadConfig(
+class AsyncThreadConfig(
     private val webDriverManager: WebDriverManager,
 ) {
     // 20250225 java robot을 쓸 필요가 없는 크롤링 사이트들을 위한 쓰레드이다.
@@ -18,7 +18,7 @@ class ScrapAsyncThreadConfig(
         ThreadPoolTaskExecutor().apply {
             corePoolSize = 10 // 항상 유지되는 최소 스레드 개수
             maxPoolSize = 20 // 최대 20개까지 확장
-            setThreadNamePrefix("headless-scrap-thread")
+            setThreadNamePrefix("headless")
             setTaskDecorator { runnable ->
                 Runnable {
                     /*
@@ -36,25 +36,26 @@ class ScrapAsyncThreadConfig(
             initialize()
         }
 
-    @Bean("un-headless")
+    @Bean("find-post")
     fun asyncThreadTaskExecutor(): ThreadPoolTaskExecutor =
         ThreadPoolTaskExecutor().apply {
             // 한 화면은 하나의 WebDriver만이 사용되어야하기떄문에 쓰레드는 한개만 유지되어야한다.
             corePoolSize = 5 // 항상 유지되는 최소 스레드 개수
             maxPoolSize = 5 //
-            setThreadNamePrefix("un-headless-scrap-thread")
+            setThreadNamePrefix("un-headless")
             setTaskDecorator { runnable ->
                 Runnable {
                     /*
                     쓰레드마다 서로 다른 WebDriver를 배정하기 위해
                     쓰레드 실행시, WebDriver등록
-                    쓰레드 실행 완료시 WebDriver제거
                      */
                     webDriverManager.setNewWebDriver(false)
                     try {
                         runnable.run()
                     } finally {
-                        webDriverManager.removeWebDriver()
+                        // WebDriver를 끄고 키는 시간이 오래걸리기에 쓰레드 실행 완료시에도
+                        // WebDriver를 쓰레드 로컬에서 제거하지 않는다.
+//                        webDriverManager.removeWebDriver()
                     }
                 }
             }
