50f11b37c5dfe950afa9c0017833ac98f985ec4a
starwook <star.wookk@gmail.com>
2025-10-28 03:00:19 +0900

    기관 분양 워선

--- api/src/main/kotlin/com/server/animalmoa/api/adoption/controller/ViewController.kt
+++ api/src/main/kotlin/com/server/animalmoa/api/adoption/controller/ViewController.kt
@@ -28,7 +28,7 @@ class ViewController(
     @GetMapping("/adoption/free")
     fun getFreeAdoptions(
         model: Model,
-        @RequestParam(value = "personal", defaultValue = true.toString()) personal: Boolean,
+        @RequestParam(value = "personal", defaultValue = false.toString()) personal: Boolean,
         @RequestParam(value = "page", defaultValue = "1") page: Int,
         @RequestParam(value = "size", defaultValue = "12") size: Int,
         @RequestParam(value = "region", defaultValue = "WIDE") region: Region,
--- crawler/src/main/kotlin/com/server/animalmoa/crawler/source/nabiya/NabiyaAdoptionHtmlParser.kt
+++
@@ -1,4 +0,0 @@
-package com.server.animalmoa.crawler.source.nabiya
-
-object NabiyaAdoptionHtmlParser {
-}
